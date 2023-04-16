45e59e94b04b50572fc599c7f2ea73b0f4fc5aa0
anjiayu9 <130870700+anjiayu9@users.noreply.github.com>
2023-04-16 12:28:26 -0400

    Update search.js

--- javascript/search.js
+++ javascript/search.js
@@ -1,53 +1,34 @@
-const apiKey = "AIzaSyA45AScMoF4lp0o8tasA_mOW1MlxgdG7FA";
+const apiKey = "AIzaSyCfTIJgyraAPohYyzkSnPJakFlNEXzD2cE";
 const engineID = "1148ff606ea12418a";
 const searchForm = document.getElementById("search-form");
 const resultsDiv = document.getElementById("results");
 
-gapi.load("client", () => {
-  gapi.client.setApiKey(apiKey);
-  gapi.client.load("https://language.googleapis.com/$discovery/rest?version=v1");
-});
+ searchForm.addEventListener("submit", async (event) => {
+     event.preventDefault();
+     const zipCode = document.getElementById("zip-code").value;
+     const query = `therapy animal near ${zipCode}`;
+     const results = await retrieveInfo(query);
+     displayResults(results);
+ });
 
-searchForm.addEventListener("submit", async (event) => {
-  event.preventDefault();
-  const zipCode = document.getElementById("zip-code").value;
-  const query = `therapy animal near ${zipCode}`;
-  const nlpQuery = await analyzeQuery(query);
-  const results = await retrieveInfo(nlpQuery);
-  displayResults(results);
-});
+ async function retrieveInfo(query) {
+     const url = `https://www.googleapis.com/customsearch/v1?key=${apiKey}&cx=${engineID}&q=${encodeURIComponent(query)}`;
+     const response = await fetch(url);
+     const data = await response.json();
 
-async function analyzeQuery(query) {
-  const response = await gapi.client.language.documents.analyzeEntities({
-    document: {
-      content: query,
-      type: "PLAIN_TEXT",
-    },
-  });
+     return data.items || [];
+ }
 
-  const entities = response.result.entities || [];
-  const processedQuery = entities.map(entity => entity.name).join(" ");
-  return processedQuery;
-}
-
-async function retrieveInfo(query) {
-  const url = `https://www.googleapis.com/customsearch/v1?key=${apiKey}&cx=${engineID}&q=${encodeURIComponent(query)}`;
-  const response = await fetch(url);
-  const data = await response.json();
-
-  return data.items || [];
-}
-
-function displayResults(results) {
-  resultsDiv.innerHTML = "";
-  results.forEach(result => {
-    const resultDiv = document.createElement("div");
-    resultDiv.classList.add("card");
-    resultDiv.innerHTML = `
-        <h3 class="card-title">${result.title}</h3>
-        <a href="${result.link}" target="_blank">${result.link}</a>
-        <p>${result.snippet}</p>
-    `;
-    resultsDiv.appendChild(resultDiv);
-  });
-}
+ function displayResults(results) {
+     resultsDiv.innerHTML = "";
+     results.forEach(result => {
+         const resultDiv = document.createElement("div");
+         resultDiv.classList.add("card");
+         resultDiv.innerHTML = `
+             <h3 class="card-title">${result.title}</h3>
+             <a href="${result.link}" target="_blank">${result.link}</a>
+             <p>${result.snippet}</p>
+         `;
+         resultsDiv.appendChild(resultDiv);
+     });
+ }
